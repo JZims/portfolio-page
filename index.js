218d0ecd2f4ea372a86c28f7291259c116fd5df9
JZims <JZimmsGames@gmail.com>
2021-03-12 01:00:08 -0500

    Added background colors to each section, highlighting different projects

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-/*const h2 = document.createElement('h2');
+const h2 = document.createElement('h2');
+//Creates a variable named h2 which holds an h2 element
 h2.textContent = "This content added by JavaScript";
+//Populates the text of our h2 element
+//must be called in the dom by:
+//document.querySelector('ul').appendChild(h2);
 
-document.querySelector('div#profile-link').appendChild(h2);*/
