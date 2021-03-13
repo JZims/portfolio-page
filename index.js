5df49aadb9de32488974aed885334f17e2050501
JZims <JZimmsGames@gmail.com>
2021-03-13 00:52:03 -0500

    Slightly closer to fixing JS, but still needs work

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-const h2 = document.createElement('h2');
+//const h2 = document.createElement('h2');
 //Creates a variable named h2 which holds an h2 element
-h2.textContent = "This content added by JavaScript";
+//h2.textContent = "This content added by JavaScript";
 //Populates the text of our h2 element
 //must be called in the dom by:
 //document.querySelector('ul').appendChild(h2);
-const titleSwap = document.getElementsByClassName('project-title');
 
-document.addEventListener('mouseover', function() {
-document.addEventListenerchangeClass='.project-title-swap';
+let titleSwap = document.querySelector('div > a.project-tile');
+
+titleSwap.addEventListener('mouseover', function(e) {
+ document.querySelector('p.project-title').className='.project-title-swap';
 });
+
+
+
